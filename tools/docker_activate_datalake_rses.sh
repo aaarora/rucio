358bc589d2d669fb32a209c955eed6e1ddf54277
Jonathan Guiang <jkguiang@gmail.com>
2022-01-07 14:45:07 -0800

    fixed

--- tools/docker_activate_datalake_rses.sh
+++ tools/docker_activate_datalake_rses.sh
@@ -18,23 +18,14 @@
 # - Radu Carpa <radu.carpa@cern.ch>, 2021
 
 # Create the following topology:
-# +------------+   1   +----------------------+
-# |            |------>|                      |
-# | T2_US_UCSD |       | DATALAKE_ORIGIN_2811 |
-# |            |--+    |                      |
-# +------------+  |    +----------------------+
-#                 |                ^
-#                 | 1              | 1
-#                 |                v
-#                 |    +----------------------+
-#                 |    |                      |
-#                 +--->| DATALAKE_ORIGIN_2812 |
-#                      |                      |
-#                      +----------------------+
+# +------------+       +----------------------+
+# |            |   1   |                      |
+# | T2_US_UCSD |------>| DATALAKE_ORIGIN_2811 |
+# |            |       |                      |
+# +------------+       +----------------------+
 
 # First, create the RSEs
 rucio-admin rse add DATALAKE_ORIGIN_2811 # Destination 1
-rucio-admin rse add DATALAKE_ORIGIN_2812 # Destination 2
 rucio-admin rse add T2_US_UCSD           # Source
 
 # Add the protocol definitions for the Data Lake server
@@ -43,11 +34,6 @@ rucio-admin rse add-protocol --hostname k8s1-pb10.ultralight.org \
     --impl rucio.rse.protocols.gfalv2.Default \
     --domain-json '{"wan": {"read": 1, "write": 1, "delete": 1, "third_party_copy": 1}, "lan": {"read": 1, "write": 1, "delete": 1}}' \
     DATALAKE_ORIGIN_2811
-rucio-admin rse add-protocol --hostname k8s1-pb10.ultralight.org \
-    --scheme https --prefix // --port 2812 \
-    --impl rucio.rse.protocols.gfalv2.Default \
-    --domain-json '{"wan": {"read": 1, "write": 1, "delete": 1, "third_party_copy": 1}, "lan": {"read": 1, "write": 1, "delete": 1}}' \
-    DATALAKE_ORIGIN_2812
 
 # Add the protocol definitions for the UCSD T2 server
 rucio-admin rse add-protocol --hostname gftp-1.t2.ucsd.edu \
@@ -58,26 +44,20 @@ rucio-admin rse add-protocol --hostname gftp-1.t2.ucsd.edu \
 
 # Set test_container_xrd attribute for xrd containers
 rucio-admin rse set-attribute --rse DATALAKE_ORIGIN_2811 --key test_container_xrd --value True
-rucio-admin rse set-attribute --rse DATALAKE_ORIGIN_2812 --key test_container_xrd --value True
 rucio-admin rse set-attribute --rse T2_US_UCSD --key test_container_xrd --value True
 
 # Enable FTS
 rucio-admin rse set-attribute --rse DATALAKE_ORIGIN_2811 --key fts --value https://fts:8446
-rucio-admin rse set-attribute --rse DATALAKE_ORIGIN_2812 --key fts --value https://fts:8446
 rucio-admin rse set-attribute --rse T2_US_UCSD --key fts --value https://fts:8446
 
 # Enable multihop transfers via DATALAKE_ORIGIN_2811
 rucio-admin rse set-attribute --rse DATALAKE_ORIGIN_2811 --key available_for_multihop --value True
-rucio-admin rse set-attribute --rse DATALAKE_ORIGIN_2812 --key available_for_multihop --value True
 
 # Connect the RSEs
 rucio-admin rse add-distance --distance 1 --ranking 1 T2_US_UCSD DATALAKE_ORIGIN_2811
-rucio-admin rse add-distance --distance 1 --ranking 1 T2_US_UCSD DATALAKE_ORIGIN_2812
-rucio-admin rse add-distance --distance 1 --ranking 1 DATALAKE_ORIGIN_2811 DATALAKE_ORIGIN_2812
 
 # Indefinite limits for root
 rucio-admin account set-limits root DATALAKE_ORIGIN_2811 -1
-rucio-admin account set-limits root DATALAKE_ORIGIN_2812 -1
 rucio-admin account set-limits root T2_US_UCSD -1
 
 # Delegate credentials to FTS
@@ -119,4 +99,3 @@ rucio attach scope$e:dataset$c scope$e:file$a scope$e:file$b
 rucio add-container scope$e:container$d
 rucio attach scope$e:container$d scope$e:dataset$c
 
-rucio add-rule scope$e:container$d 1 DATALAKE_ORIGIN_2812
