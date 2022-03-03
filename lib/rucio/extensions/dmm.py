849f045c50b2c9b89de6c13e6c5ab4ad41ea6c04
Jonathan Guiang <jkguiang@gmail.com>
2022-03-03 11:03:29 -0800

    fixed

--- lib/rucio/extensions/dmm.py
+++ lib/rucio/extensions/dmm.py
@@ -2,70 +2,65 @@
 SENSE Optimizer Prototype
 """
 
-import requests
 import logging
 from multiprocessing.connection import Client
+from rucio.core.rse import get_rse_name
 
 cache = {}
 
 ADDRESS = ("localhost", 5000)
 AUTHKEY = b"secret password"
 
-def sense_finisher(replicas):
+def sense_finisher(rule_id, replicas):
     """
-    Parse replicas and update SENSE on how many jobs (per source+dest RSE pair) have finished via DMM
+    Parse replicas and update SENSE on how many jobs (per source+dest RSE pair) have 
+    finished via DMM
 
+    :param rule_id:     Rucio rule ID
     :param replicas:    Individual replicas produced by now-finished transfers
     """
     finisher_reports = {}
     for replica in replicas:
-        rule_id = replica["rule_id"] # TODO: check if this works
         if rule_id not in finisher_reports.keys():
             finisher_reports[rule_id] = {
-                "n_transfers_finished": 1,
-                "n_bytes_transferred": replica["bytes"]
+                "n_transfers_finished": 0,
+                "n_bytes_transferred": 0
             }
-        else:
-            finisher_reports[rule_id]["n_transfers_finished"] += 1
-            finisher_reports[rule_id]["n_bytes_transferred"] += replica["bytes"]
+        finisher_reports[rule_id]["n_transfers_finished"] += 1
+        finisher_reports[rule_id]["n_bytes_transferred"] += replica["bytes"]
 
     with Client(ADDRESS, authkey=AUTHKEY) as client:
         client.send(("FINISHER", finisher_reports))
-
 
 def sense_updater(results_dict):
     print(results_dict)
 
 def sense_preparer(requests_with_sources):
     """
-    Parse RequestWithSources objects collected by the preparer daemon and communicate relevant info to
-    SENSE via DMM
+    Parse RequestWithSources objects collected by the preparer daemon and communicate 
+    relevant info to SENSE via DMM
 
-    :param requests_with_sources:    Individual file rse_pairs (see rucio.transfer.RequestWithSource)
+    :param requests_with_sources:    List of rucio.transfer.RequestWithSource objects
     """
-    # Collect requested rse_pairs
     prepared_rules = {}
     for rws in requests_with_sources:
-        # Update metadata
         if rws.rule_id not in prepared_rules.keys():
-            # Initialize metadata
             prepared_rules[rws.rule_id] = {
-                "src_rse_name": rws.sources[0].rse.name, # FIXME: can we indeed just take the first one?
-                "dst_rse_name": rws.dest_rse.name, # TODO: check that this and line above work
+                "src_rse_name": rws.sources[0].rse.name, # FIXME: can we always take the first one?
+                "dst_rse_name": get_rse_name(rws.dest_rse.id),
                 "attr": {
-                    "rucio_rule_id": rws.rule_id,
-                    "rucio_request_id": rws.request_id,
+                    "request_ids": [],
                     "priority": rws.attributes["priority"],
                     "n_transfers_total": 0,
                     "n_bytes_total": 0
                 }
             }
+        prepared_rules[rws.rule_id]["attr"]["request_ids"].append(rws.request_id)
         prepared_rules[rws.rule_id]["attr"]["n_transfers_total"] += 1
         prepared_rules[rws.rule_id]["attr"]["n_bytes_total"] += rws.byte_count
 
-    # Communicate the collected information to SENSE via DMM
     with Client(ADDRESS, authkey=AUTHKEY) as client:
-        client.send(("PREPARER", prepared_jobs))
+        client.send(("PREPARER", prepared_rules))
 
 def sense_optimizer(grouped_jobs):
     """
@@ -79,7 +74,7 @@ def sense_optimizer(grouped_jobs):
     for external_host in grouped_jobs:
         for job in grouped_jobs[external_host]:
             for file_data in job["files"]:
-                rule_id = file_data["rule_id"] # TODO: check that this works
+                rule_id = file_data["rule_id"]
                 if rule_id not in submitter_reports.keys():
                     submitter_reports[rule_id] = 0
                 submitter_reports[rule_id] += 1
@@ -87,7 +82,7 @@ def sense_optimizer(grouped_jobs):
     for external_host in grouped_jobs:
         for job in grouped_jobs[external_host]:
             for file_data in job["files"]:
-                rule_id = file_data["rule_id"] # TODO: check that this works
+                rule_id = file_data["rule_id"]
                 # Retrieve SENSE mapping
                 if rule_id not in cache.keys():
                     __update_cache_with_sense_optimization(
@@ -101,7 +96,7 @@ def sense_optimizer(grouped_jobs):
                 src_sense_url = src_url.replace(src_hostname, sense_map[src_name], 1)
                 file_data["sources"][0] = (src_name, src_sense_url, src_id, src_retries)
                 # Update destination
-                dst_name = file_data["metadata"]["dest_rse"] # TODO: check that this works
+                dst_name = file_data["metadata"]["dst_rse"]
                 dst_url = file_data["destinations"][0]
                 dst_hostname = __get_hostname(dst_url)
                 dst_sense_url = dst_url.replace(dst_hostname, sense_map[dst_name], 1)
@@ -112,18 +107,18 @@ def __get_hostname(uri):
     # TODO: Need to make more universal for other url formats.
     return uri.split("//")[1].split(":")[0]
 
-def __update_cache_with_sense_optimization(priority, rse_pair_id, n_transfers_submitted):
+def __update_cache_with_sense_optimization(rule_id, n_transfers_submitted):
     """ Fetch and cache SENSE mappings via DMM """
     global cache
     with Client(ADDRESS, authkey=AUTHKEY) as client:
-        payload = {
-            "rucio_rule_id": rse_pair_id, 
+        submitter_report = {
+            "rucio_rule_id": rule_id,
             "n_transfers_submitted": n_transfers_submitted
         }
-        client.send(("SUBMITTER", payload))
+        client.send(("SUBMITTER", submitter_report))
         response = client.recv()
 
     if rule_id not in cache.keys():
-        cache[rule_id] = response.json()
+        cache[rule_id] = response
     else:
-        cache[rule_id].update(response.json())
+        cache[rule_id].update(response)
